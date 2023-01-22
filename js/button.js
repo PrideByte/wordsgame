607e8a2a4e02a83a9f4c56e8500277961e4b81fe
PrideByte <alpha5.2@mail.ru>
2023-01-22 22:47:12 +0300

    feat: add localstorage stats

--- js/button.js
+++ js/button.js
@@ -8,7 +8,14 @@ export default class letterButton {
         this.element = document.createElement('div');
         this.element.className = 'gameKeyboard__key';
         control && this.element.classList.add('gameKeyboard__key-special');
-        this.element.innerText = letter.toUpperCase();
+        if (/^[а-я]$/i.test(this.letter.toLowerCase())) {
+            this.element.innerText = letter.toUpperCase();
+        } else if (this.letter.toLowerCase() === 'backspace') {
+            this.element.innerText = 'b';
+        } else if (this.letter.toLowerCase() === 'enter') {
+            this.element.innerText = 'e';
+        }
+            
         this.parent.appendChild(this.element);
         this.callback = () => {};
         this.element.onclick = this.click.bind(this);
