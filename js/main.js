512bc65e4756d883c9727b544a3accdc80b3cf61
PrideByte <alpha5.2@mail.ru>
2023-01-22 22:50:24 +0300

    feat: add game version check

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 import game from './game.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    if (JSON.parse(localStorage.getItem('version')) !== 'v2') {
+        localStorage.clear();
+        localStorage.setItem('version', JSON.stringify('v2'));
+    }
     let g = new game(document.body);
 });
