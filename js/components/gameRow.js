a8b064e19c2c8a1c9db84a16e97d09bbe37a8d12
PrideByte <alpha5.2@mail.ru>
2023-02-16 11:25:34 +0300

    feat: small css tweaks

--- js/components/gameRow.js
+++ js/components/gameRow.js
@@ -12,9 +12,10 @@ export default class Row extends Element {
         for (let i = 0; i < wordLength; i++) {
             const letter = new Element({
                 parent: this.element,
-                classNames: 'gameField__letter'
+                classNames: 'gameField__letter',
             });
             letter.element.onclick = null;
+            letter.element.style.setProperty('--index', i);
         }
 
         this.setBgColor = this.setBgColor.bind(this);
@@ -27,5 +28,11 @@ export default class Row extends Element {
 
     setText(elNumber, text) {
         this.element.children[elNumber].innerText = text;
+    }
+
+    victoryAnimation() {
+        [...this.element.children].forEach(element => {
+            element.classList.add('gameField__letter_victory');
+        });
     }
 }
